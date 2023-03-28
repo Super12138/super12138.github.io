daae28eb5564e792e6e1547b2fa2a1be68148adb
Super12138 <70494801+Super12138@users.noreply.github.com>
2023-03-28 21:03:57 +0800

    Add files via upload

--- blog/.vitepress/config.js
+++ blog/.vitepress/config.js
@@ -1,1 +1,30 @@
+import { defineConfig } from 'vitepress'
 
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Super12138's Blog",
+  description: "Super12138's Blog",
+  base: "/",
+  lang: "zh-CN",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '博客', link: '/blog-welcome' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Super12138/super12138.github.io/' }
+    ]
+  }
+})
