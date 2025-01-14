3eb599ecf4e9cca7db9bdb82209e7410f1f431de
Super12138 <70494801+Super12138@users.noreply.github.com>
2025-01-14 16:49:23 +0800

    更新Hash Checker文档和依赖版本

--- blog/.vitepress/config.ts
+++ blog/.vitepress/config.ts
@@ -1,83 +1,83 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Super12138",
-  description: "Super12138 应用",
-  base: "/",
-  lang: "zh-CN",
-  lastUpdated: true,
-  head: [
-    [
-      'link', { rel: 'icon', href: '/Super12138.png' }
-    ]
-  ],
-  themeConfig: {
-    nav: [
-      { text: '主页', link: '/' },
-      {
-        text: '应用',
-        items: [
-          { text: '全部', link: '/apps' },
-          { text: 'Hash Checker', link: '/hschecker/intro' },
-          { text: '待办', link: '/todo/intro' },
+    title: "Super12138",
+    description: "Super12138 应用",
+    base: "/",
+    lang: "zh-CN",
+    lastUpdated: true,
+    head: [
+        [
+            'link', { rel: 'icon', href: '/Super12138.png' }
         ]
-      }
     ],
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            {
+                text: '应用',
+                items: [
+                    { text: '全部', link: '/apps' },
+                    { text: 'Hash Checker', link: '/hschecker/intro' },
+                    { text: '待办', link: '/todo/intro' },
+                ]
+            }
+        ],
 
-    sidebar: [
-      { text: '应用', link: '/apps' },
-      {
-        text: 'Hash Checker',
-        items: [
-          { text: '简介', link: '/hschecker/intro' },
-          { text: '下载', link: '/hschecker/download' },
-          { text: '使用', link: '/hschecker/use' },
-        ]
-      },
-      {
-        text: '待办',
-        items: [
-          { text: '简介', link: '/todo/intro' },
-          { text: '下载', link: '/todo/download' },
-        ]
-      }
-    ],
+        sidebar: [
+            { text: '应用', link: '/apps' },
+            {
+                text: 'Hash Checker',
+                items: [
+                    { text: '简介', link: '/hschecker/intro' },
+                    { text: '下载', link: '/hschecker/download' },
+                    { text: '使用', link: '/hschecker/use' },
+                ]
+            },
+            {
+                text: '待办',
+                items: [
+                    { text: '简介', link: '/todo/intro' },
+                    { text: '下载', link: '/todo/download' },
+                ]
+            }
+        ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/Super12138/super12138.github.io/' }
-    ],
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/Super12138/super12138.github.io/' }
+        ],
 
-    footer: {
-      copyright: `版权所有 © 2019-${new Date().getFullYear()} Super12138`
-    },
+        footer: {
+            copyright: `版权所有 © 2019-${new Date().getFullYear()} Super12138`
+        },
 
-    lastUpdated: {
-      text: '最后更新于',
-      formatOptions: {
-        dateStyle: 'short',
-        timeStyle: 'medium'
-      }
-    },
+        lastUpdated: {
+            text: '最后更新于',
+            formatOptions: {
+                dateStyle: 'short',
+                timeStyle: 'medium'
+            }
+        },
 
-    docFooter: {
-      prev: '上一页',
-      next: '下一页'
-    },
+        docFooter: {
+            prev: '上一页',
+            next: '下一页'
+        },
 
-    editLink: {
-      pattern: 'https://github.com/Super12138/super12138.github.io/edit/main/blog/:path',
-      text: '在 GitHub 上编辑此页面'
-    },
+        editLink: {
+            pattern: 'https://github.com/Super12138/super12138.github.io/edit/main/blog/:path',
+            text: '在 GitHub 上编辑此页面'
+        },
 
-    outline: {
-      label: '页面导航'
-    },
+        outline: {
+            label: '页面导航'
+        },
 
-    returnToTopLabel: '回到顶部',
-    sidebarMenuLabel: '菜单',
-    darkModeSwitchLabel: '主题',
-    lightModeSwitchTitle: '切换到浅色模式',
-    darkModeSwitchTitle: '切换到深色模式'
-  }
-})
+        returnToTopLabel: '回到顶部',
+        sidebarMenuLabel: '菜单',
+        darkModeSwitchLabel: '主题',
+        lightModeSwitchTitle: '切换到浅色模式',
+        darkModeSwitchTitle: '切换到深色模式'
+    }
+});
--- blog/hschecker/download.md
+++ blog/hschecker/download.md
@@ -1,41 +1,50 @@
 # 下载
 Hash Checker 支持 Windows 和 macOS 双平台，你可以在下方找到 Windows 和 macOS 应用的下载链接
 
-## Windows <Badge type="tip" text="2.0.1" />
+## Windows <Badge type="tip" text="2.2.1" />
 ::: warning
-Windows 支持 Windows 7 或更高版本，如要在 Windows 7 以下旧版本请尝试使用网页版
+Windows 支持 Windows 10 或更高版本和 64 位架构。如要在 Windows 10 以下旧版本或 32 位、arm64架构的电脑上使用，请尝试使用[网页版](https://super12138.github.io/Hash-Checker/)
 请根据你的设备选择您要下载的版本
 :::
-- [Windows x64](https://github.com/Super12138/Hash-Checker/releases/download/2.0.1/Hash.Checker.Setup.2.0.1.exe)
-- [Windows x32](https://github.com/Super12138/Hash-Checker/releases/download/2.0.1/Hash.Checker.Setup.2.0.1.exe)
-- [Windows arm64](https://github.com/Super12138/Hash-Checker/releases/download/2.0.1/Hash.Checker.Setup.2.0.1-arm64.exe)
+- [Windows x64 安装器版](https://github.com/Super12138/Hash-Checker/releases/download/2.2.1/Hash.Checker-installer.exe)
+- [Windows x64 便携版](https://github.com/Super12138/Hash-Checker/releases/download/2.2.1/Hash.Checker-portable.exe)
 
-## macOS <Badge type="tip" text="2.0.1" />
+## macOS <Badge type="tip" text="2.2.1" />
 ::: warning
 macOS 支持 macOS 10.11 (El Capitan) 以及更高版本，并且支持 Apple Silicon (arm64) 设备
+<br>
 请根据你的设备选择您要下载的版本
 :::
-- [macOS Universal（通用芯片）](https://github.com/Super12138/Hash-Checker/releases/download/2.0.1/Hash.Checker-2.0.1-universal.dmg)
-- [macOS（Intel 芯片版）](https://github.com/Super12138/Hash-Checker/releases/download/2.0.1/Hash.Checker-2.0.1.dmg)
-- [macOS arm64（Apple 芯片版）](https://github.com/Super12138/Hash-Checker/releases/download/2.0.1/Hash.Checker-2.0.1-arm64.dmg)
 
-## 网页版 <Badge type="tip" text="2.2.1" />
+::: tip
+**已知问题**
+<br>
+当前 `2.2.1` 版本由于使用 `Wails` 框架在打包上传时出现应用损坏，无法打开，故暂不提供下载方式，敬请谅解。
+<br>
+macOS 用户如需使用可以使用[网页版](https://super12138.github.io/Hash-Checker/)，或前往 [Github Actions](https://github.com/Super12138/Hash-Checker/actions/workflows/build.yml) 下载使用 `Tauri` 框架构建的版本
+:::
+
+## 网页版 <Badge type="tip" text="2.3.0" />
 ::: warning
-本应用理论支持所有主流浏览器的最新版本，在某些过旧版本上会出现无法计算 Hash 值的情况。推荐您使用 Chromium 内核（如：Chrome（Chromium）、Edge）的浏览器进行访问，以确保您的体验。网页版支持PWA。
+本应用理论支持所有主流浏览器的较新版本。由于使用了 Web 的新特性，所以在某些过旧版本的浏览器上会出现无法计算 Hash 值的情况。
+<br>
+推荐您使用基于 [Chromium](https://www.chromium.org/) 内核构建的浏览器（如 [Chrome](https://www.google.cn/chrome/)、[Edge](https://www.microsoft.com/zh-cn/edge/download) 等）进行访问，以确保您的体验。
+<br>
+网页版支持PWA
 :::
 - [点击此处前往网页版](https://super12138.github.io/Hash-Checker/)
 
-## Beta <Badge type="tip" text="2.2.0" />
-这个版本是测试版本，相比正式版更不稳定，但包含了最新的相比 CI 版较为稳定的功能更新。
+## Beta
+此版本是测试版本，相比正式版更不稳定。但它包含了比 Dev 版更稳定的功能和安全更新
 
 ::: warning
 请注意，某些新功能可能会在正式版中移除，请根据自身需求下载
 :::
 
-[前往 Github Releases](https://github.com/Super12138/Hash-Checker/releases/)
+目前暂无 `Beta` 版
 
 ## Dev
-这个版本是预览版本，它由 GitHub Actions 构建的版本，相比 Beta 版**更不稳定**。<br>
+此版本是预览版本，它由 GitHub Actions 构建的版本，相比 Beta 版**更不稳定**。<br>
 它在每次提交代码后构建，具有最新的功能和修复，但也具有很强的不稳定性，可能出现某些影响使用体验的 bug。<br>
 如遇问题，可在 [GitHub Issues](https://github.com/Super12138/Hash-Checker/issues) 上反馈，并**写清版本号**（可在【设置】→【关于】→【版本】一行查看）以便确定问题所在分支；同时也请您**准确描述问题**以更好复现并修复。
 
